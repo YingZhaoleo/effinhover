c4f53550a7c6e0f2a637fc0327522c067d29afba
hovercraft <mauro.pfister@epfl.ch>
2019-03-29 20:39:15 +0100

    Pushed wrong test_communication.py before

--- ground/test_communication.py
+++ ground/test_communication.py
@@ -4,126 +4,96 @@ import socket
 import sys
 import math
 import rospy
+import csv
 import drone_pb2 as drone
 
 # Get inspired by : https://github.com/germain-hug/Autonomous-RC-Car/blob/master/scripts/manual_driver.py
 
-def setupUDP():
-	ground = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
 
-	ground_address = ('0.0.0.0', 3333)
-	print >> sys.stderr, 'starting up on %s port %s' % ground_address
-	ground.bind(ground_address)
+class Controller(object):
+	""" Controller Node
+	Generates motor control and sends them to the hovercraft
+	"""
 
-	while True:
-		print >>sys.stderr, '\nwaiting to receive message'
-		_ , hover_address = ground.recvfrom(4096)
+	def __init__(self):
+
+		# Set up ground socket
+		self.ground = 	ground = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.ground_address = ground_address = ('0.0.0.0', 3333)
+		print >> sys.stderr, 'starting up on %s port %s' % ground_address
+		self.ground.bind(ground_address)
+
+		# Try to connect to hovercraft
+		print("Connecting to hovercraft ...")
+		_ , self.remote_address = ground.recvfrom(1024)
 		
-		if (hover_address != 0):
-			print >>sys.stderr, 'received answer from %s' % hover_address
-			return ground, hover_address
-    
-  
-def setupNode():
-	rospy.init_node('controller')
-	r = rospy.Rate(0.5)
-
-thrust = 1
-lift = 0
-
-motors = drone.Motors()
-
-#motors.motor_DL = (int)(1000 + 1000 * lift)
-#motors.motor_DR = motors.motor_DL
-#motors.motor_L = (int)(1000 + thrust * 500)
-#motors.motor_R = (int)(1000 + thrust * 500)
-
-motors.motor_DL = 1000
-motors.motor_DR = 1000
-motors.motor_L = 1000
-motors.motor_R = 1000
-
-counter = 0
-
-'''
-def controll_callback(event):
-    print 'Timer called at ' + str(event.current_real)
-	data = motors.SerializeToString()
-	sent = ground.sendto(data, hover_address)
-'''
-
-def controller():
-	#rospy.Timer(rospy.Duration(2), controll_callback)
-	while not rospy.is_shutdown():
-		if (counter % 2 == 0):
-			motors.motor_R = 2000
-			motors.motor_L = 1000
+		if (self.remote_address != 0):
+			print("Connected successfully")
 		else:
-			motors.motor_L = 2000
-			motors.motor_R = 1000
-		r.sleep()
+			print("Could not connect to hovercraft")
+
+		# Start up rospy node
+		rospy.init_node('Controller')
+		rospy.on_shutdown(self.stop)	# Stop motors when shutdown
+
+		self.rate = rospy.Rate(10)	# 10 Hz
+
+		self.motors = drone.Motors()
+
+		self.motors.motor_DL = 2000
+		self.motors.motor_DR = 2000
+		self.motors.motor_L = 1000
+		self.motors.motor_R = 1000
+
+		self._armed = False
+		self._thrust = 0.6
+		self._lift = 1.0
+
+		self._yawrate_ref = 0
+
+		self.ud_prev = 0
+		self.y_prev = 0
+
+
+	def sendToMotors(self):
+		self.ground.sendto(self.motors.SerializeToString(), self.remote_address)
+
+
+	def controlLoop(self):
+		#rospy.Timer(rospy.Duration(2), controll_callback)
+		# Open and read prbs file
+		prbs = []
+		with open('/home/prabhat/hovercraft_ws/src/ground/src/prbs','r') as f:
+			read = csv.reader(f)
+			for row in read:
+				prbs.append(row)\
+
+		prbs = iter(prbs)
+
+		# Currently input.csv is written to hovercraft_ws
+		with open('input.csv', 'w') as f:
+			writer = csv.writer(f)		# Ugly way of logging data -> better use separate node
+
+			while not rospy.is_shutdown():
+				self.motors.motor_R = int(next(prbs) [0])
+				self.motors.motor_L = int(next(prbs) [1])
+
+				self.sendToMotors()
+				data = [rospy.get_time(), self.motors.motor_R, self.motors.motor_L]
+				#print(rospy.get_time())
+				writer.writerow(data)
+				self.rate.sleep()
+		
+
+	def stop(self):
+		print("\nShut down and stop motors")
+		self.motors.motor_DL = 1000
+		self.motors.motor_DR = 1000
+		self.motors.motor_L = 1000
+		self.motors.motor_R = 1000
+		self.sendToMotors()
 
 
 if __name__ == "__main__":
-	ground, hover_address = setupUDP()
-	setupNode()
-	contoller()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-
-def control_loop(self, state, prbs):
-		self.motors.motor_DL = (int)(1000 + 1000 * self.lift)
-		self.motors.motor_DR = self.motors.motor_DL
-
-		# Compute PD controller
-		y = state.gyro_z / 1000
-		ud = 0.606531 * self.ud_prev + 11.000000 * y + -10.606531 * self.y_prev
-		self.ud_prev = ud
-		self.y_prev = y
-
-		Kp = 1/2
-		u = Kp * (self._yawrate_ref - ud)
-
-		# print('u = %f\r' % u)
-		u = max(min(u, 1), -1)
-
-		thrust = self.thrust
-
-		self.motors.motor_L = (int)(1000 + thrust * 500)
-		self.motors.motor_R = (int)(1000 + thrust * 500)
-
-		if u > 0:
-			self.motors.motor_L = self.motors.motor_L + (int)(500 * u)
-		else:
-			self.motors.motor_R = self.motors.motor_L - (int)(500 * u)
-
-'''
-
+	controller = Controller()
+	controller.controlLoop()
