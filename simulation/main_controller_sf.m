1ce518319e857f9f49b96696e7775adda192fb67
kabdelaziz <48073634+kabdelaziz@users.noreply.github.com>
2019-05-25 12:09:57 +0200

    Trajectory bug fixes, still not working as intended

--- simulation/main_controller_sf.m
+++ simulation/main_controller_sf.m
@@ -6,13 +6,14 @@ close all
 clc
 
 %% Simulation settings
-h = 0.005;          % Sample time
-tmax = 20;          % Simulation time   
+h = 0.01;           % Sample time
+tmax = 150;         % Simulation time   
 t = 0:h:tmax;       % Time vector
 
 % Remember to set same parameters in dynamics function
 m = 17.6;
 Iz = 1.98;
+
 % Neglect damping effects
 d11 = 0;
 d22 = 0;
@@ -27,39 +28,47 @@ xr = R*cos(2*pi*psir);
 yr = R*sin(2*pi*psir);
 
 % Trajectory derivatives
-xr_d = R*2*pi*w*(-sin(2*pi*psir));
-yr_d = R*2*pi*w*cos(2*pi*psir);
+xr_d = R*2*pi*w * (-sin(2*pi*psir));
+xr_dd = R*(2*pi*w)^2 *cos(2*pi*psir);
+
+yr_d = R*2*pi*w * cos(2*pi*psir);
+yr_dd = R*(2*pi*w)^2 * sin(2*pi*psir);
 
 % Desired velocities and yaw rate with derivatives
-ur = 0.05;
-rr = 0.05;
+ur = xr_d.*cos(2*pi*psir) + yr_d.*sin(2*pi*psir); % ur = 0.05;
+vr = -xr_d.*sin(2*pi*psir) + yr_d.*cos(2*pi*psir);
+
+rr = -0.05;
 ur_d = 0;
 rr_d = 0;
 
-% Reference sway, obtained by rearranging xr_d
-vr = (1./sin(2*pi*psir)) .* (ur*cos(2*pi*psir)-xr_d);
-vr(1) = 0;          % Override division by 0 in first term
+% Reference sway, obtained by rearranging xr_d (initial attempt, not
+% working)
+% vr = (1./sin(2*pi*psir)) .* (ur*cos(2*pi*psir)-xr_d);
+% vr(1) = 0;        % Override division by 0 in first term
 
-% Controller reference terms
+% Controller reference terms (suggestion but not working)
+%u1r = (xr_dd.*cos(2*pi*psir) + xr_d.*2*pi*rr.*sin(2*pi*psir) + yr_dd.*sin(2*pi*psir)...
+    %+ yr_d*2*pi*rr.*cos(2*pi*psir) - vr*rr + d11*ur) * m;
+
 u1r = -m*vr*rr + d11*ur;
-u2r = -(m-m)*ur*vr + d33*rr;
+u2r = -(m-m).*ur.*vr + d33*rr;
 
 % Max input to system
 u1_max = 0.9;
 u2_max = 0.9;
+%% Simulate system
 
 % Initial control input
 U = [0; 0];
 
-%% Simulate system
-
 % Initial state vector
 X0 = zeros(6, 1);
-X0(1) = xr(1);%4.75;
+X0(1) = -1;%4.75;
 X0(2) = yr(1);%3.5;
 X0(3) = pi;
-X0(4) = ur;
-X0(5) = 0;
+X0(4) = ur(1);
+X0(5) = vr(1);
 X0(6) = rr;
 
 rk4.name = 'RK4';
@@ -71,21 +80,21 @@ for k = 1:length(t) - 1
     X_d = dynamics(rk4.X(:,k), U, 0);
         
     if (mod(k,1) == 0)
-        U = trajectory_controller_sf(X_d,xr(k),yr(k),psir(k),ur,...
+        U = trajectory_controller_sf(X_d,xr(k),yr(k),psir(k),ur(k),...
             vr(k),rr,u1r(k),u2r(k));
         U(1) = min(max(U(1), 0), u1_max); 
         U(2) = min(max(U(2), -u2_max), u2_max);
         U = [U(1);U(2)];
     end
-    
+
     rk4.U(:, k) = U;
     rk4.X(:,k+1) = rk4.f_discrete(rk4.X(:,k), U);
 end
 
 %% Visualise and plot results
-visualize(t, rk4.X, 20)
-
-% Actual and reference trajectories
+ visualize(t, rk4.X, 20)
+ 
+%% Actual and reference trajectories
 figure
 hold on
 plot(rk4.X(1, :), rk4.X(2, :), 'b')
--- simulation/trajectory_controller_sf.m
+++ simulation/trajectory_controller_sf.m
@@ -1,4 +1,4 @@
-function U = trajectory_controller_sf(X_d,xr,yr,psir,ur,vr,rr,u1r,u2r);
+function [U ue ve re z1e z2e z3e] = trajectory_controller_sf(X_d,xr,yr,psir,ur,vr,rr,u1r,u2r);
 %TRAJECTORY_CONTROLLER_SF position tracking control of underactuated hovercraft.
 %   Reference paper: "Tracking Control of an Underactuated Ship" by Erjen
 %   Lefeber Kristin Pattersen and Henk Nijmeijer.
@@ -32,19 +32,20 @@ function U = trajectory_controller_sf(X_d,xr,yr,psir,ur,vr,rr,u1r,u2r);
     ve = v - vr;
     re = r - rr;
     
-    z1 = x*cos(psi) + y*sin(psi);
-    z1r = xr*cos(psir) + yr*sin(psir);
+    z1 = x*cos(2*pi*psi) + y*sin(2*pi*psi);
+    z1r = xr*cos(2*pi*psir) + yr*sin(2*pi*psir);
     
-    z2 = -x*sin(psi) + y*cos(psi);
-    z2r = -xr*sin(psir) + yr*cos(psir);
+    z2 = -x*sin(2*pi*psi) + y*cos(2*pi*psi);
+    z2r = -xr*sin(2*pi*psir) + yr*cos(2*pi*psir);
 
 % Position and angle errors    
-    z1e = z1 - z1r;
-    z2e = z2 - z2r;
+    z1e = z1 - z1r; %x - xr;
+    z2e = z2 - z2r; %y - yr;
     z3e = psi - psir;
 
-% Compute control inputs, preassigned gains
-    U(1) = u1r - 10.28*ue + 9.2*ve - 4.44*z1e + 2.74*z2e;
-    U(2) = u2r - 9.02*re - 6.74*z3e;
+% Compute control inputs, preassigned gains (open loop by commenting after
+% u1r, u2r)
+    U(1) = u1r; % - 10.28*ue + 9.2*ve - 4.44*z1e + 2.74*z2e;
+    U(2) = u2r; % - 9.02*re - 6.74*z3e;
     
 end
