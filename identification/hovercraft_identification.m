0bda677c230c5c87f9650a36471e6e8c51cc4236
Mauro Pfister <maurofabio@bluewin.ch>
2019-06-11 16:02:17 +0200

    Adapt identifiation procedure to new data

--- identification/hovercraft_identification.m
+++ identification/hovercraft_identification.m
@@ -1,76 +1,62 @@
 %% Hovercraft parameter identification - Experiments
 %  main program for fitting parameters of ODE hovercraft model to data
 %  objective function is defined in objFun.m
-%  dynamic model is defined in dynamicsId.m
 %
 
 clear
 clc
 close all
 
+% import functions from simulation folder
+addpath('../simulation');
+
+
 % path to identification data
-data_id_path = '../../hovercraft-ros/data_id/';
+data_id_path = 'identification_data/set01/';
 
-poses = dir([data_id_path, 'pose_2019-04-16*.csv']);
-controls = dir([data_id_path, 'controls_2019-04-16*.csv']);
+eta_file = dir([data_id_path, 'eta*.csv']);
+nu_file = dir([data_id_path, 'nu*.csv']);
+ctrl_file = dir([data_id_path, 'controls*.csv']);
 
 
-%poses = dir([data_id_path, 'pose_2019-04-16-12-29-28.csv']);
+n_files = length(eta_file);
 
-%controls = dir([data_id_path, 'controls_2019-04-16-12-29-28.csv']);
-
-n_files = length(poses);
-
-theta_estim = zeros(n_files, 5);    % save parameters
+theta_estim = zeros(n_files, 4);    % save parameters
 
 % load data for every run
-for n = 1:6
-    pose = readtable([data_id_path, poses(n).name]);
-    ctrl = readtable([data_id_path, controls(n).name]);
+for n = 1:n_files
     
-    t_pose = pose.field_header_stamp';
-    x = pose.field_point_x';
-    y = pose.field_point_y';
-    psi = pose.field_point_z';
-    psi = unwrap(psi);                   % eliminate jumps and make psi continuous
+    % import data as tables
+    eta_read = readtable([data_id_path, eta_file(n).name]);
+    nu_read = readtable([data_id_path, nu_file(n).name]);
+    ctrl_read = readtable([data_id_path, ctrl_file(n).name]);
+    
+    t_eta = eta_read.field_header_stamp';   % same time for all imported variables
+    t_eta = (t_eta - t_eta(1)) / 1e9;       % set start time to 0 and convert to seconds
+    eta = [eta_read.field_point_x, eta_read.field_point_y, eta_read.field_point_z]';
+    nu = [nu_read.field_point_x, nu_read.field_point_y, nu_read.field_point_z]';
+    ctrl = [ctrl_read.field_point_x, ctrl_read.field_point_y]';
+    
+    %psi = pose.field_point_z';
+    %psi = unwrap(psi);                   % eliminate jumps and make psi continuous
     %psi = medfilt1(psi, 10);             % eliminate spikes 
     
-    t_ctrl = ctrl.field_header_stamp';
-    u_L = ctrl.field_point_x';
-    u_R = ctrl.field_point_y';
-    u = [u_L; u_R];
-    
-    % find start time and convert from nanoseconds to seconds
-    t_start = min(t_pose(1), t_ctrl(1));
-    t_pose = (t_pose - t_start)/1e9;
-    t_ctrl = (t_ctrl - t_start)/1e9;
-    t_ctrl(1) = 0;
-    
+    %nu(2,:) = medfilt1(nu(2,:), 3, [], 2);   % filter nu to eliminate spikes
+
+   
     % downsample measurements
-    t_skip = 10;
-    t_start = 5; % timedelay
-    t_end = 500; %length(t_pose);
-    t_pose = t_pose(t_start:t_skip:t_end);
-    x = x(t_start:t_skip:t_end);
-    y = y(t_start:t_skip:t_end);
-    psi = psi(t_start:t_skip:t_end);
+    %t_skip = 10;
+    %t_start = 5; % timedelay
+    %t_end = 500; %length(t_pose);
+    %t_pose = t_pose(t_start:t_skip:t_end);
+    %x = x(t_start:t_skip:t_end);
+    %y = y(t_start:t_skip:t_end);
+    %psi = psi(t_start:t_skip:t_end);
         
-    eta = [x; y; psi];
+    %eta_file = [x; y; psi];
 
-    %visualize(t_pose, eta, 1);
+    %visualize(t_eta, eta, 1);
 
-    
-    % velocities in world frame
-    vel_world = diff(eta, 1, 2)./diff(t_pose);
-    
-    % calculate velocities (u, v, r) in body frame
-    nu = zeros(size(vel_world));
-    for k = 1:(length(t_pose) - 1) 
-        R_z_psi_T = [cos(eta(3, k)), sin(eta(3, k)), 0;
-                   -sin(eta(3, k)),  cos(eta(3, k)), 0;
-                         0     ,       0     , 1];
-        nu(:,k) = R_z_psi_T * vel_world(:, k);          
-    end
     
 %     figure
 %     plot(nu')
@@ -78,77 +64,81 @@ for n = 1:6
 %     hold on
 %     plot(nu')
 %     
-    dataId.t_nu = t_pose(1:end-1);
+    dataId.t_nu = t_eta;
     dataId.nu = nu;
-    dataId.t_U = t_ctrl;
-    dataId.U = u;
+    dataId.t_U = t_eta;
+    dataId.U = ctrl;
     dataId.h = 0.005;
     
-    theta0 = [0.08, 0.016, 1, 1, 0.0005];
+    theta0 = [0.8, 0.016, 0.05, 0.005];
     
-    [thetamin, objmin] = paramId(dataId, theta0)
+    [thetamin, objmin] = param_id(dataId, theta0)
     theta_estim(n, :) = thetamin;
     
     
     % Plot simulated trajectory with estimated parameters
     
-    % Simulation setup
-    h = 0.005;           % sampling time
-    tmax = t_pose(end);     % simulation time   
-    t = 0:h:tmax;         % simulation time 
-    X0 = [eta(:,1); nu(:,1)];       % initial condition
+    plot_stuff = true;
+    
+    if (plot_stuff)
+        % Simulation setup
+        h = 0.005;           % sampling time
+        tmax = t_eta(end);     % simulation time   
+        t = 0:h:tmax;         % simulation time 
+        X0 = [eta(:,1); nu(:,1)];       % initial condition
 
-    % Upsampling input signal for simulation with nearest interpolation
-    Usim = interp1(t_ctrl, u', t, 'previous', 'extrap');
-    Usim = Usim';
-    
-    
-    dynamics =@(X, U) dynamicsParam(X, U, thetamin);
+        % Upsampling input signal for simulation with nearest interpolation
+        Usim = interp1(t_eta, ctrl', t, 'previous', 'extrap');
+        Usim = Usim';
 
-    % RK4 discrete system = RK4 function with function handle (@dynamics) of dynamics function
-    sim.f_discrete = @(X,U) RK4(X, U, h, dynamics);
-    sim.X = X0;
-    
-    for k = 1:length(t) - 1
-      sim.X(:,k+1) = sim.f_discrete(sim.X(:,k), Usim(:,k));
+
+        dynamics_param =@(X, U) dynamics(X, U, [0.058, thetamin, 0.0325]);
+
+        % RK4 discrete system = RK4 function with function handle (@dynamics) of dynamics function
+        sim.f_discrete = @(X,U) RK4(X, U, h, dynamics_param);
+        sim.X = X0;
+
+        for k = 1:length(t) - 1
+          sim.X(:,k+1) = sim.f_discrete(sim.X(:,k), Usim(:,k));
+        end
+
+
+        %visualize(t(:), sim.X(:,:), 1);
+
+
+        figure
+        hold on
+        plot(sim.X(1,:), sim.X(2,:));
+        plot(eta(1,:), eta(2,:));
+        legend('sim', 'meas');
+        set(gca, 'YDir','reverse')      % Flip direction of y-axis to match coordinate system
+        axis equal
+        xlabel('x [m]')
+        ylabel('y [m]')
+
+        figure
+        subplot(3,1,1)
+        hold on
+        plot(t, sim.X(4,:));
+        plot(t_eta, nu(1,:));
+        legend('sim', 'meas');
+        xlabel('t [s]')
+        ylabel('u [m/s]')
+        subplot(3,1,2)
+        hold on
+        plot(t, sim.X(5,:));
+        plot(t_eta, nu(2,:));
+        legend('sim', 'meas');
+        xlabel('t [s]')
+        ylabel('v [m/s]')
+        subplot(3,1,3)
+        hold on
+        plot(t, sim.X(6,:));
+        plot(t_eta, nu(3,:));
+        legend('sim', 'meas');
+        xlabel('t [s]')
+        ylabel('r [rad/s]')
+
+        drawnow
     end
-    
-    
-    %visualize(t(:), sim.X(:,:), 1);
-
-    
-    figure
-    hold on
-    plot(sim.X(1,:), sim.X(2,:));
-    plot(eta(1,:), eta(2,:));
-    legend('sim', 'meas');
-    set(gca, 'YDir','reverse')      % Flip direction of y-axis to match coordinate system
-    axis equal
-    xlabel('x [m]')
-    ylabel('y [m]')
-
-    figure
-    subplot(3,1,1)
-    hold on
-    plot(t, sim.X(4,:));
-    plot(t_pose(1:end-1), nu(1,:));
-    legend('sim', 'meas');
-    xlabel('t [s]')
-    ylabel('u [m/s]')
-    subplot(3,1,2)
-    hold on
-    plot(t, sim.X(5,:));
-    plot(t_pose(1:end-1), nu(2,:));
-    legend('sim', 'meas');
-    xlabel('t [s]')
-    ylabel('v [m/s]')
-    subplot(3,1,3)
-    hold on
-    plot(t, sim.X(6,:));
-    plot(t_pose(1:end-1), nu(3,:));
-    legend('sim', 'meas');
-    xlabel('t [s]')
-    ylabel('r [rad/s]')
-    
-    drawnow
 end
